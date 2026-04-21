e6febc8bd5b063c3c2209d295b40e4ab15b016a0
avamvakas02 <a.vamvakas@acg.edu>
2026-04-22 01:01:30 +0300

    Improved the homepage and added more features

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views # Built-in Auth
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('collection/', views.collection, name='collection'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    
+    # Login and Logout paths
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]
